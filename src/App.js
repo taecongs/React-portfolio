ebe7f7bc4be98e0a253a696d4d8c5ac16959c2ec
taecongs <bks0927w@gmail.com>
2022-12-05 19:00:03 +0900

    Project : 포트폴리오 작업

--- src/App.js
+++ src/App.js
@@ -18,8 +18,12 @@ const App = () => {
         // console.log("onLeave", { origin, destination, direction });
         // console.log('99999', destination.anchor);
 
-        let $line = document.querySelector('.header_gnb li::after');
-        console.log('***********************************', $line);
+        // let $line = document.querySelectorAll('.header_gnb li');
+        // let $line = document.querySelector('.header_gnb li::after');
+        // console.log($line);
+
+
+        let $li = document.querySelectorAll('.header_gnb li');
 
         let $nav = document.querySelectorAll('.header_gnb li a');
 
@@ -29,15 +33,50 @@ const App = () => {
         if(destination.anchor === 'firstPage'){
             $nav.forEach(($nav_a) => { $nav_a.style.color = "#fff";});
             $dot.forEach(($nav_a) => { $nav_a.style.background = "#fff";});
-            // $line.forEach(($nav_a) => {$nav_a.style.background = '#fff'});
+            $li[0].className = 'header_on0';
+            $li[1].classList.remove('header_on1');
+            $li[2].classList.remove('header_on2');
+            $li[3].classList.remove('header_on3');
+
+
         } else if(destination.anchor === 'secondPage'){
             $nav.forEach(($nav_a) => { $nav_a.style.color = "#000";});
             $dot.forEach(($nav_a) => { $nav_a.style.background = "#000";});
-            // $line.forEach(($nav_a) => {$nav_a.style.background = '#000'});
+            $li[0].classList.remove('header_on0');
+            $li[2].classList.remove('header_on2');
+            $li[3].classList.remove('header_on3');
+
+            $li[0].className = 'header_line';
+            $li[2].className = 'header_line';
+            $li[3].className = 'header_line';
+
+            $li[1].className = 'header_on1';
+
+
         } else if(destination.anchor === 'thirdPage'){
             $nav.forEach(($nav_a) => { $nav_a.style.color = "#000";});
+            $li[0].classList.remove('header_on0');
+            $li[1].classList.remove('header_on1');
+            $li[3].classList.remove('header_on3');
+
+            $li[0].className = 'header_line';
+            $li[1].className = 'header_line';
+            $li[3].className = 'header_line';
+
+            $li[2].className = 'header_on2';
+
+
         } else if(destination.anchor === 'fourPage'){
             $nav.forEach(($nav_a) => { $nav_a.style.color = "#000";});
+            $li[0].classList.remove('header_on0');
+            $li[1].classList.remove('header_on1');
+            $li[2].classList.remove('header_on2');
+
+            $li[0].className = 'header_line';
+            $li[1].className = 'header_line';
+            $li[2].className = 'header_line';
+
+            $li[3].className = 'header_on3';
         }
     }
     return (
