a12078cee9b30819b65d782c80a34e1d6cda7074
taecongs <bks0927w@gmail.com>
2022-12-13 21:03:55 +0900

    Project : 2022-12-13 포트폴리오 작업

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -134,13 +134,13 @@ const Contact = () => {
 
                                     <div className='contact_link'>
                                         <div className="notion_box2">
-                                            <a href="https://abundant-cyclamen-76a.notion.site/01b71b9d5b0e48b9b0ae85d3ce8366b9" target="_blank">
+                                            <a href="https://abundant-cyclamen-76a.notion.site/01b71b9d5b0e48b9b0ae85d3ce8366b9" target="_blank" rel='noopener noreferrer'>
                                                 <span className="notion_text">노션 바로가기</span>
                                             </a>
                                         </div>
 
                                         <div className="git_box2">
-                                            <a href="https://github.com/taecongs" target="_blank">
+                                            <a href="https://github.com/taecongs" target="_blank" rel='noopener noreferrer'>
                                                 <span className="git_text">깃헙 바로가기</span>
                                             </a>
                                         </div>
