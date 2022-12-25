66940ec5ca22c6f489739a186b60f35bbdb229dd
taecongs <bks0927w@gmail.com>
2022-12-25 16:59:51 +0900

    Project 포트폴리오 수정 작업

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ const App = () => {
         let $dot = document.querySelectorAll('#fp-nav ul li a span, .fp-slidesNav ul li a span');
         // let $top = document.querySelector('#top_btn');
 
+        $dot.forEach(($nav_a) => { $nav_a.style.background = "#fff"; });
+
         if (destination.anchor === 'home') {
             // top 버튼            
             // $top.style.display = 'none';
